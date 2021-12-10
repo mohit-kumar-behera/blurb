a8877cd27b01fdfbc5a80e0e9be55b5b14018428
mohit-kumar-behera <mohitkumar.b3399@gmail.com>
2021-12-10 12:48:58 +0530

    add navlink style

--- blurb-client/src/components/App.jsx
+++ blurb-client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // import components
 import Navbar from './Navbar/Navbar';
@@ -6,8 +7,74 @@ import Navbar from './Navbar/Navbar';
 // import stylesheets
 import './App.css';
 
+const Comp1 = () => {
+  return <h1>Component 1</h1>;
+};
+const Comp2 = () => {
+  return <h1>Component 2</h1>;
+};
+const Comp3 = () => {
+  return <h1>Component 3</h1>;
+};
+
 const App = () => {
-  return <Navbar />;
+  return (
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Comp1 />} />
+          <Route path="/comp2" exact element={<Comp2 />} />
+          <Route path="/comp3" exact element={<Comp3 />} />
+        </Routes>
+      </>
+    </Router>
+  );
 };
 
 export default App;
+
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+// export default function App() {
+//   return (
+//     <Router>
+//       <div>
+//         <nav>
+//           <ul>
+//             <li>
+//               <Link to="/">Home</Link>
+//             </li>
+//             <li>
+//               <Link to="/about">About</Link>
+//             </li>
+//             <li>
+//               <Link to="/users">Users</Link>
+//             </li>
+//           </ul>
+//         </nav>
+
+//         {/* A <Switch> looks through its children <Route>s and
+//             renders the first one that matches the current URL. */}
+//         <Routes>
+//           <Route path="/" exact element={<Home />}></Route>
+//           <Route path="/about" element={<About />}></Route>
+//           <Route path="/users" element={<Users />}></Route>
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// function Home() {
+//   return <h2>Home</h2>;
+// }
+
+// function About() {
+//   return <h2>About</h2>;
+// }
+
+// function Users() {
+//   return <h2>Users</h2>;
+// }
--- blurb-client/src/components/Navbar/Navbar.jsx
+++ blurb-client/src/components/Navbar/Navbar.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import h from '../../components/icon.png';
+import blurb_icon from '../../static/blurb-icon.png';
 import './Navbar.css';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="one">
-        <span>ALL</span>
+      <div className="left">
+        <Link to="/" className="link navlink">
+          Home
+        </Link>
       </div>
-      <div className="two">
+      <div className="middle">
         <img
-          src={h}
+          src={blurb_icon}
           width="30"
           height="30"
           style={{ transform: 'scale(2.5)' }}
         />
       </div>
-      <div className="three">
-        <div>Log in</div>
-        <div>Log out</div>
+      <div className="right">
+        <Link to="/comp2" className="link navlink">
+          Login
+        </Link>
+        <Link to="/comp3" className="link navlink">
+          Logout
+        </Link>
       </div>
     </nav>
   );
