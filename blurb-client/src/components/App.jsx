c6c19b8f6b2e7b9c14eca3a9e508f1a010774e6b
mohit-kumar-behera <mohitkumar.b3399@gmail.com>
2021-12-11 13:18:25 +0530

    BrowserRouter is mapped to <App/>

--- blurb-client/src/components/App.jsx
+++ blurb-client/src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet, Link, useParams } from 'react-router-dom';
 
 // import components
 import Navbar from './Navbar/Navbar';
@@ -25,22 +25,82 @@ const Comp3 = () => {
   return (
     <div className="component-wrapper">
       <h1>Component 3</h1>
+      <Link to="new">New</Link>
+      <Link to="edit">Edit</Link>
+      <Link to="delete">Delete</Link>
+      <Link to="edit/798798798">Edit exact</Link>
+      <hr />
+      <hr />
+      <Outlet />
+    </div>
+  );
+};
+
+const Comp3Home = () => {
+  return (
+    <div className="component-wrapper">
+      <h1>Component 3 Home ppage</h1>
+      <hr />
+    </div>
+  );
+};
+
+const Comp3New = () => {
+  return (
+    <div className="component-wrapper">
+      <h1>Component 3 new ppage</h1>
+      <hr />
+    </div>
+  );
+};
+
+const Comp3Edit = () => {
+  return (
+    <div className="component-wrapper">
+      <h1>Component 3 edit ppage</h1>
+      <hr />
+      <Outlet />
+    </div>
+  );
+};
+
+const Comp3Delete = () => {
+  return (
+    <div className="component-wrapper">
+      <h1>Component 3 delte ppage</h1>
+      <hr />
+    </div>
+  );
+};
+
+const Comp3EditExact = () => {
+  const { id } = useParams();
+
+  return (
+    <div className="component-wrapper">
+      <h1>Component 3 edit ID: {id} page</h1>
+      <hr />
     </div>
   );
 };
 
 const App = () => {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Routes>
-          <Route path="/" exact element={<Comp1 />} />
-          <Route path="/comp2" exact element={<Comp2 />} />
-          <Route path="/comp3" exact element={<Comp3 />} />
-        </Routes>
-      </>
-    </Router>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Comp1 />} />
+        <Route path="/comp2" exact element={<Comp2 />} />
+        <Route path="/comp3" exact element={<Comp3 />}>
+          <Route index element={<Comp3Home />} />
+          <Route path="new" exact element={<Comp3New />} />
+          <Route path="edit" exact element={<Comp3Edit />}>
+            <Route path=":id" exact element={<Comp3EditExact />} />
+          </Route>
+          <Route path="delete" exact element={<Comp3Delete />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
 
