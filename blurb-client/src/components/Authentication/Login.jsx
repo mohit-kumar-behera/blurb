b9212367a8a13b6e2a2622f41f54e436cebfeee8
mohit-kumar-behera <mohitkumar.b3399@gmail.com>
2021-12-18 13:44:08 +0530

    basic validation and rendering using redux form

--- blurb-client/src/components/Authentication/Login.jsx
+++ blurb-client/src/components/Authentication/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 // import components
 import Input from './Fields/Input';
@@ -6,37 +7,88 @@ import Input from './Fields/Input';
 // import custom hooks
 import useDocumentTitle from '../../CustomHook/useDocumentTitle';
 
+const inputComponents = {
+  email: (
+    <Input
+      type="email"
+      placeholder="johndoe567@gmail.com"
+      className="user-input"
+      spellCheck="false"
+      required
+    />
+  ),
+  password: (
+    <Input
+      type="password"
+      placeholder="************"
+      className="user-input"
+      spellCheck="false"
+      required
+    />
+  ),
+};
+
+const FormField = formProps => {
+  const { input: inputProps, meta, type, label } = formProps;
+
+  const renderInput = (inputType, meta) => {
+    const fakeInput = Object.assign({}, inputComponents[inputType]);
+    fakeInput.props = { ...fakeInput.props, ...inputProps };
+    return fakeInput;
+  };
+
+  const renderError = ({ error, touched }) => {
+    let errorObj = { errMsg: '', errCls: '' };
+
+    if (touched && error)
+      errorObj = { ...errorObj, errMsg: error, errCls: 'show' };
+    else errorObj = { ...errorObj, errMsg: '', errCls: '' };
+
+    return (
+      <small className={`error-label ${errorObj.errCls}`}>
+        {errorObj.errMsg}
+      </small>
+    );
+  };
+
+  return (
+    <div className="form-field">
+      <label className="label">{label}</label>
+      {renderInput(type.toLowerCase(), meta)}
+      {renderError(meta)}
+    </div>
+  );
+};
+
 const Login = props => {
   useDocumentTitle(props.title);
+
+  const submitHandler = formValues => {
+    console.log('values', formValues);
+  };
 
   return (
     <>
       <h1 className="form-head">Log into your account</h1>
-      <form method="post" className="form">
+      <form
+        method="post"
+        className="form"
+        onSubmit={props.handleSubmit(submitHandler)}
+      >
+        <Field
+          name="email"
+          component={FormField}
+          label="Email Address"
+          type="email"
+        />
+        <Field
+          name="password"
+          component={FormField}
+          label="Password"
+          type="password"
+        />
         <div className="form-field">
-          <label className="label">Email Address</label>
-          <Input
-            type="email"
-            placeholder="johndoe567@gmail.com"
-            className="user-input"
-            spellCheck="false"
-            required
-          />
-          <small className="error-label">This is error</small>
-        </div>
-        <div className="form-field">
-          <label className="label">Password</label>
-          <Input
-            type="password"
-            placeholder="************"
-            className="user-input"
-            spellCheck="false"
-            required
-          />
-          <small className="error-label show">This is error</small>
-        </div>
-        <div className="form-field">
-          <button type="button" className="form-action-btn">
+          <button type="submit" className="form-action-btn">
             Login
           </button>
         </div>
@@ -45,4 +97,21 @@ const Login = props => {
   );
 };
 
-export default Login;
+const validate = formValues => {
+  const errors = {};
+  const requiredFields = ['email', 'password'];
+
+  requiredFields.forEach(requiredField => {
+    if (!formValues[requiredField])
+      errors[
+        requiredField
+      ] = `${requiredField.toUpperCase()} field can't be left empty.`;
+  });
+
+  return errors;
+};
+
+export default reduxForm({
+  form: 'LoginForm',
+  validate,
+})(Login);
