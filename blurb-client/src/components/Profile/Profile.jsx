6b541307c2b6ce38740c9a0e1d75164e265f6d3c
mohit-kumar-behera <mohitkumar.b3399@gmail.com>
2022-02-07 12:38:12 +0530

    updates

--- blurb-client/src/components/Profile/Profile.jsx
+++ blurb-client/src/components/Profile/Profile.jsx
@@ -17,32 +17,6 @@ const Profile = props => {
       <div className="ovenlayout-detail-header">
         <h1 className="profile-header">Profile Page</h1>
       </div>
-      {/* <div className="ovenlayout-detail-header">
-        <Avataar
-          src={default_pfpic}
-          alt_text="Default Profile Picture"
-          avataar_classes={['profile-pic']}
-          rounded={true}
-        />
-      </div>
-      <div className="ovenlayout-detail-header user-detail-wrapper">
-        <div className="user-detail">
-          <strong className="detail-key">First Name</strong>
-          <p className="detail-value">Mohit</p>
-        </div>
-        <div className="user-detail">
-          <strong className="detail-key">Last Name</strong>
-          <p className="detail-value">Kumar</p>
-        </div>
-        <div className="user-detail">
-          <strong className="detail-key">Email</strong>
-          <p className="detail-value">mohit@mail.com</p>
-        </div>
-        <div className="user-detail">
-          <strong className="detail-key">Name</strong>
-          <p className="detail-value">Mohit Kumar</p>
-        </div>
-      </div> */}
 
       <div className="ovenlayout-detail-body user-detail-wrapper">
         <div className="user-detail__left">
